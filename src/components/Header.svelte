45285fe69ed0f7708e27ed0750e03fc3adcd5c1e
Script Raccoon <scriptraccoon@gmail.com>
2022-09-16 14:38:57 +0200

    add github repo link

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,16 @@
+<script>
+    import Fa from "svelte-fa";
+    import { faGithub } from "@fortawesome/free-brands-svg-icons";
+</script>
+
 <header>
+    <a
+        href="https://github.com/ScriptRaccoon/GetThisDone"
+        target="_blank"
+        title="GitHub repository"
+    >
+        <Fa icon={faGithub} />
+    </a>
     <h1>Get this done</h1>
 </header>
 
@@ -8,8 +20,15 @@
         padding: 20px 0px;
         background: var(--primary-color);
         color: white;
+        position: relative;
     }
     h1 {
         font-weight: 400;
+    }
+    a {
+        position: absolute;
+        color: inherit;
+        top: 5px;
+        right: 8px;
     }
 </style>
